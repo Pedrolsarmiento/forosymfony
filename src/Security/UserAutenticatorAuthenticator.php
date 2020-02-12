25f03046c9d5c8c5561332b5c1e6652c6262cee9
Pedrostaldone <pedrostaldone@gmail.com>
2020-02-13 00:56:31 +0100

    solucionado redirect del login

--- src/Security/UserAutenticatorAuthenticator.php
+++ src/Security/UserAutenticatorAuthenticator.php
@@ -92,10 +92,12 @@ class UserAutenticatorAuthenticator extends AbstractFormLoginAuthenticator imple
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
+
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('ultimas_publicaciones'));
     }
 
     protected function getLoginUrl()
